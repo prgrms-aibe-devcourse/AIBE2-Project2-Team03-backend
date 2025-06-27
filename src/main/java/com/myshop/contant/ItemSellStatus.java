dafaeb4eddd30e23fed1033d114e973db673875c
INUK-ai <arheon2469@gmail.com>
2025-06-27 16:25:06 +0900

    setting: spring-3

---
+++ src/main/java/com/myshop/contant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.myshop.contant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT  // 품절
+}
---
+++ src/main/java/com/myshop/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.myshop.repository;
+
+import com.myshop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemName(String itemName);
+}
