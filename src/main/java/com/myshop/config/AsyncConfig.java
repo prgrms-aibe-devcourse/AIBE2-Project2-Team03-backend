4dc0247b6af0c34e24687004fe1ebe7154f9b811
INUK-ai <arheon2469@gmail.com>
2025-07-13 21:20:02 +0900

    feat: AsyncConfig - threadPoolTaskExecutor

--- src/main/java/com/myshop/config/AsyncConfig.java
+++ src/main/java/com/myshop/config/AsyncConfig.java
@@ -1,9 +1,24 @@
 package com.myshop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // 기본 스레드 풀 크기
+        executor.setMaxPoolSize(20); // 최대 스레드 풀 크기
+        executor.setQueueCapacity(50); // 큐 용량
+        executor.setThreadNamePrefix("Async-"); // 스레드 이름 접두사
+        executor.initialize();
+        return executor;
+    }
 }
