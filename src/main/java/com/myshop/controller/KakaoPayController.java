41293f4babc9b41a17d98c178b94214f0adb583b
INUK-ai <arheon2469@gmail.com>
2025-07-14 17:38:54 +0900

    feat: KakaoPayController - acceptPay 후 slack message 이벤트 발행

--- src/main/java/com/myshop/controller/KakaoPayController.java
+++ src/main/java/com/myshop/controller/KakaoPayController.java
@@ -2,12 +2,16 @@ package com.myshop.controller;
 
 import com.myshop.dto.KakaoPayApproveDto;
 import com.myshop.dto.KakaoPayReadyDto;
+import com.myshop.notification.domain.message.PaymentSuccessMessage;
+import com.myshop.notification.event.NotificationEvent;
 import com.myshop.service.KakaoPayService;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,11 +21,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class KakaoPayController {
   private final KakaoPayService kakaoPayService;
+  private final ApplicationEventPublisher applicationEventPublisher;
   
   @GetMapping(value = "/kakaopay/success")
   public String acceptPay(@RequestParam("pg_token") String pgToken) {
     KakaoPayApproveDto result = kakaoPayService.acceptPay(pgToken);
-    // send slack message
+    NotificationEvent notificationEvent = new NotificationEvent(
+        result.getPartner_user_id(),
+            new PaymentSuccessMessage(
+                    result.getPartner_user_id(),
+                    result.getTotalAmount(),
+                    result.getPartner_order_id()
+            )
+    );
+    applicationEventPublisher.publishEvent(notificationEvent);
     return "redirect:/";
   }
 }
